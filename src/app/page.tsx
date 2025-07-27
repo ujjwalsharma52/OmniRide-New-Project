08bf3a8e542a47de7d26cd7fce056f9a965bc1ce
Ujjwal Kumar <ujjwalsharmasharma09@gmail.com>
2025-07-27 17:48:25 +0000

    remove 600x400

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,6 @@
 "use client";
 
 import { useState } from "react";
-import Image from "next/image";
 import { Separator } from "@/components/ui/separator";
 import VehicleSuggestionForm from "@/components/vehicle-suggestion-form";
 import VehicleOptions from "@/components/vehicle-options";
@@ -12,18 +11,8 @@ import { RatingDialog } from "@/components/rating-dialog";
 export default function Home() {
 
   return (
-    <div className="h-[calc(100vh-4rem)] flex flex-col lg:flex-row">
-       <section className="flex-1 w-full lg:h-full h-1/2 relative">
-        <Image 
-          src="https://placehold.co/600x400.png" 
-          alt="Map placeholder" 
-          layout="fill" 
-          objectFit="cover"
-          data-ai-hint="city map"
-        />
-        <div className="absolute inset-0 bg-black/20" />
-      </section>
-      <aside className="lg:w-[450px] lg:h-full flex flex-col border-t lg:border-t-0 lg:border-l bg-card overflow-y-auto h-1/2">
+    <div className="flex justify-center items-start">
+      <aside className="w-full max-w-lg lg:h-full flex flex-col bg-card overflow-y-auto">
         <div className="p-6">
           <VehicleSuggestionForm />
         </div>
